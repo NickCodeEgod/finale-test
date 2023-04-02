fa18384191ef882770d97b42d6ff899fff29a182
NickCodeEgod <123401332+NickCodeEgod@users.noreply.github.com>
2023-04-02 17:24:05 +0400

    a

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+const email = document.getElementById('email');
+  const emailConfirm = document.getElementById('email-confirm');
+  const error = document.getElementById('email-confirm-error');
+  const submitBtn = document.getElementById('submit-btn');
+  
+  function validateEmail() {
+    if (email.value !== emailConfirm.value) {
+      error.style.display = 'inline';
+      submitBtn.disabled = true;
+    } else {
+      error.style.display = 'none';
+      submitBtn.disabled = false;
+    }
+  }
+  
+  email.addEventListener('input', validateEmail);
+  emailConfirm.addEventListener('input', validateEmail);
+
